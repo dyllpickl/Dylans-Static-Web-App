f366b886cd53b3dcb9acbbb76406008b550bf683
dyllpickl <144762997+dyllpickl@users.noreply.github.com>
2023-11-12 16:40:58 -0800

    Update lab6.js

--- src/cs212/Lab/6/lab6.js
+++ src/cs212/Lab/6/lab6.js
@@ -1,3 +1,6 @@
-function product(x, y) {
-  var result = x * y;
+var value1 = parseInt(document.getElementbyId('value1').value, 10);
+var value2 = parseInt(document.getElementbyId('value2').value, 10);
+
+function product() {
+  var result = value1 * value2;
   document.getElementbyId('product_of_2').innerHTML = "The product is: " + result;}
