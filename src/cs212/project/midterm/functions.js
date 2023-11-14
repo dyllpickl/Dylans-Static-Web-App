d42fce0dce1264f6514280e4e935815c9a5f3b8f
dyllpickl <144762997+dyllpickl@users.noreply.github.com>
2023-11-13 20:09:07 -0800

    Update functions.js

--- src/cs212/project/midterm/functions.js
+++ src/cs212/project/midterm/functions.js
@@ -1,10 +1,10 @@
-function start_game() {
+function start_game (){
   document.getElementById("story").innerHTML = start_of_game.introduction;
   document.getElementById("question").innerHTML = start_of_game.begin_prompt;
   document.getElementById("choice1_button").value = "Begin";
-  document.getElementById("choice1_button").onclick = route1();}
+  document.getElementById("choice1_button").onclick = route1;}
 
-function route1() {
+function route1 (){
   document.getElementById("story").innerHTML = stage1_2.backstory;
   document.getElementById("question").innerHTML = stage1_2.question;
   document.getElementById("choice1_button").value = stage1.choice1;
@@ -12,116 +12,116 @@ function route1() {
   document.getElementById("choice2_button").disabled = false;
   document.getElementById("choice2_button").style.display="inline";
   document.getElementById("choice2_button").value = stage1.choice2;
-  document.getElementById("choice2_button").onclick = route1_2();}
+  document.getElementById("choice2_button").onclick = route1_2;}
 
-function route1_1() {
+function route1_1 (){
   document.getElementById("story").innerHTML = stage1_1.backstory;
   document.getElementById("question").innerHTML = stage1_1.question;
   document.getElementById("choice1_button").value = stage1_1.choice1;
-  document.getElementById("choice1_button").onclick = route1_1_1();
+  document.getElementById("choice1_button").onclick = route1_1_1;
   document.getElementById("choice2_button").value = stage1_1.choice2;
-  document.getElementById("choice2_button").onclick = route1_1_2();}
+  document.getElementById("choice2_button").onclick = route1_1_2;}
 
-function route1_2() {
+function route1_2 (){
   document.getElementById("story").innerHTML = stage1_2.backstory;
   document.getElementById("question").innerHTML = stage1_2.question;
   document.getElementById("choice1_button").value = stage1_2.choice1;
-  document.getElementById("choice1_button").onclick = route1_2_1();
+  document.getElementById("choice1_button").onclick = route1_2_1;
   document.getElementById("choice2_button").value = stage1_2.choice2;
-  document.getElementById("choice2_button").onclick = route1_2_2();}
+  document.getElementById("choice2_button").onclick = route1_2_2;}
 
-function route1_1_1() {
+function route1_1_1 (){
   document.getElementById("story").innerHTML = stage1_1_1.backstory;
   document.getElementById("question").innerHTML = stage1_1_1.question;
   document.getElementById("choice1_button").value = stage1_1_1.choice1;
-  document.getElementById("choice1_button").onclick = end_route1_1_1_1();
+  document.getElementById("choice1_button").onclick = end_route1_1_1_1;
   document.getElementById("choice2_button").value = stage1_1_1.choice2;
-  document.getElementById("choice2_button").onclick = end_route1_1_1_2();}
+  document.getElementById("choice2_button").onclick = end_route1_1_1_2;}
 
-function route1_1_2() {
+function route1_1_2 (){
   document.getElementById("story").innerHTML = stage1_1_2.backstory;
   document.getElementById("question").innerHTML = stage1_1_2.question;
   document.getElementById("choice1_button").value = stage1_1_2.choice1;
-  document.getElementById("choice1_button").onclick = end_route1_1_2_1();
+  document.getElementById("choice1_button").onclick = end_route1_1_2_1;
   document.getElementById("choice2_button").value = stage1_1_2.choice2;
-  document.getElementById("choice2_button").onclick = end_route1_1_2_2();}
+  document.getElementById("choice2_button").onclick = end_route1_1_2_2;}
 
-function route1_2_1() {
+function route1_2_1 (){
   document.getElementById("story").innerHTML = stage1_2_1.backstory;
   document.getElementById("question").innerHTML = stage1_2_1.question;
   document.getElementById("choice1_button").value = stage1_2_1.choice1;
-  document.getElementById("choice1_button").onclick = end_route1_2_1_1();
+  document.getElementById("choice1_button").onclick = end_route1_2_1_1;
   document.getElementById("choice2_button").value = stage1_2_1.choice2;
-  document.getElementById("choice2_button").onclick = end_route1_2_1_2();}
+  document.getElementById("choice2_button").onclick = end_route1_2_1_2;}
 
-function route1_2_2() {
+function route1_2_2 (){
   document.getElementById("story").innerHTML = stage1_2_2.backstory;
   document.getElementById("question").innerHTML = stage1_2_2.question;
   document.getElementById("choice1_button").value = stage1_2_2.choice1;
-  document.getElementById("choice1_button").onclick = end_route1_2_2_1();
+  document.getElementById("choice1_button").onclick = end_route1_2_2_1;
   document.getElementById("choice2_button").value = stage1_2_2.choice2;
-  document.getElementById("choice2_button").onclick = end_route1_2_2_2();}
+  document.getElementById("choice2_button").onclick = end_route1_2_2_2;}
 
-function ending1_1_1_1() {
+function ending1_1_1_1 (){
   document.getElementById("story").innerHTML = ending1_1_1_1;
   document.getElementById("question").innerHTML = start_of_game.restart_prompt;
   document.getElementById("choice1_button").value = "Restart";
-  document.getElementById("choice1_button").onclick = start_game();
+  document.getElementById("choice1_button").onclick = start_game;
   document.getElementById("choice2_button").disabled = true;
   document.getElementById("choice2_button").style.display="none";}
 
-function ending1_1_1_2() {
+function ending1_1_1_2 (){
   document.getElementById("story").innerHTML = ending1_1_1_2;
   document.getElementById("question").innerHTML = start_of_game.restart_prompt;
   document.getElementById("choice1_button").value = "Restart";
-  document.getElementById("choice1_button").onclick = start_game();
+  document.getElementById("choice1_button").onclick = start_game;
   document.getElementById("choice2_button").disabled = true;
   document.getElementById("choice2_button").style.display="none";}
 
-function ending1_1_2_1() {
+function ending1_1_2_1 (){
   document.getElementById("story").innerHTML = ending1_1_2_1;
   document.getElementById("question").innerHTML = start_of_game.restart_prompt;
   document.getElementById("choice1_button").value = "Restart";
-  document.getElementById("choice1_button").onclick = start_game();
+  document.getElementById("choice1_button").onclick = start_game;
   document.getElementById("choice2_button").disabled = true;
   document.getElementById("choice2_button").style.display="none";}
 
-function ending1_1_2_2() {
+function ending1_1_2_2 (){
   document.getElementById("story").innerHTML = ending1_1_2_2;
   document.getElementById("question").innerHTML = start_of_game.restart_prompt;
   document.getElementById("choice1_button").value = "Restart";
-  document.getElementById("choice1_button").onclick = start_game();
+  document.getElementById("choice1_button").onclick = start_game;
   document.getElementById("choice2_button").disabled = true;
   document.getElementById("choice2_button").style.display="none";}
 
-function ending1_2_1_1() {
+function ending1_2_1_1 (){
   document.getElementById("story").innerHTML = ending1_2_1_1;
   document.getElementById("question").innerHTML = start_of_game.restart_prompt;
   document.getElementById("choice1_button").value = "Restart";
-  document.getElementById("choice1_button").onclick = start_game();
+  document.getElementById("choice1_button").onclick = start_game;
   document.getElementById("choice2_button").disabled = true;
   document.getElementById("choice2_button").style.display="none";}
 
-function ending1_2_1_2() {
+function ending1_2_1_2 (){
   document.getElementById("story").innerHTML = ending1_2_1_2;
   document.getElementById("question").innerHTML = start_of_game.restart_prompt;
   document.getElementById("choice1_button").value = "Restart";
-  document.getElementById("choice1_button").onclick = start_game();
+  document.getElementById("choice1_button").onclick = start_game;
   document.getElementById("choice2_button").disabled = true;
   document.getElementById("choice2_button").style.display="none";}
 
-function ending1_2_2_1() {
+function ending1_2_2_1 (){
   document.getElementById("story").innerHTML = ending1_2_2_1;
   document.getElementById("question").innerHTML = start_of_game.restart_prompt;
   document.getElementById("choice1_button").value = "Restart";
-  document.getElementById("choice1_button").onclick = start_game();
+  document.getElementById("choice1_button").onclick = start_game;
   document.getElementById("choice2_button").disabled = true;
   document.getElementById("choice2_button").style.display="none";}
 
-function ending1_2_2_2() {
+function ending1_2_2_2 (){
   document.getElementById("story").innerHTML = ending1_2_2_2;
   document.getElementById("question").innerHTML = start_of_game.restart_prompt;
   document.getElementById("choice1_button").value = "Restart";
-  document.getElementById("choice1_button").onclick = start_game();
+  document.getElementById("choice1_button").onclick = start_game;
   document.getElementById("choice2_button").disabled = true;
   document.getElementById("choice2_button").style.display="none";}
