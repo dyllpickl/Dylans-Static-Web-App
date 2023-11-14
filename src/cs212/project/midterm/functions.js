fa709a48dcf46ddb71449fcd26bf26102307634b
dyllpickl <144762997+dyllpickl@users.noreply.github.com>
2023-11-13 18:34:03 -0800

    Update functions.js

--- src/cs212/project/midterm/functions.js
+++ src/cs212/project/midterm/functions.js
@@ -1,2 +1,8 @@
-document.getElementById("story").textContent = stage1.backstory
-document.getElementById("question").textContent = stage1.question
+var story_el = document.getElementById("story");
+var question_el = document.getElementById("question");
+story_el.textContent = start_of_game.introduction;
+question_el.textContent = start_of_game.begin_prompt;
+
+function start_game() {
+  story_el.textContent = start_of_game.introduction;
+  question_el.textContent = start_of_game.begin_prompt;}
