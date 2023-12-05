189a5dfc5e378a0829df1cdbe950140bcb8d8abf
dyllpickl <144762997+dyllpickl@users.noreply.github.com>
2023-12-04 22:20:44 -0800

    Update pwdCheck.js

--- src/cs212/LAB/lab8/pwdCheck.js
+++ src/cs212/LAB/lab8/pwdCheck.js
@@ -4,11 +4,13 @@
     var pwd = document.getElementById('pwd');
     var conf_pwd = document.getElementById('conf_pwd');
     if (pwd.value != conf_pwd.value) {
-      console.log(pwd.value == conf_pwd.value);
       return false;
     } else {
       return true;
     }
+    console.log('pwd      = ' + pwd.value);
+    console.log('conf_pwd = ' + conf_pwd.value);
+    console.log(pwd.value == conf_pwd.value);
   }
 
   // On submit, check if passwords match, if not, prevent submit
