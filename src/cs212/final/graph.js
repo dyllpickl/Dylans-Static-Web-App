09a6ece71fa2671f1009f606dabace7dcde58da2
dyllpickl <144762997+dyllpickl@users.noreply.github.com>
2023-12-10 02:34:06 -0800

    Update graph.js

--- src/cs212/final/graph.js
+++ src/cs212/final/graph.js
@@ -1,6 +1,6 @@
 (function () {
   $('td').each(function() {
-    if (Math.random() == 0) {
+    if (Math.random() < 0.5) {
       $(this).addClass('white');
     } else {
       $(this).addClass('grey');
