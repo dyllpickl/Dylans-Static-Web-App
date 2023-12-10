45f437ad9c56c7346519268865be20b836adba36
dyllpickl <144762997+dyllpickl@users.noreply.github.com>
2023-12-09 21:53:18 -0800

    Update graph.js

--- src/cs212/project/final/graph.js
+++ src/cs212/project/final/graph.js
@@ -8,8 +8,25 @@
       $(el).addClass('white');
     }
   };
-  
+
+  function allGrey() {
+    var elements = document.querySelectorAll('td');
+    var allGrey = false;
+    for (var i = 0; i < elements.length; i++) {
+      if (elements[i].hasClass('white')) {
+        allGrey = false;
+        break;
+      }
+      allGrey = true;
+    }
+    return allGrey;
+  }
+      
+                                 
   $('td').on('click', function() {
     changeClass(this);
+    if (allGrey()) {
+      window.alert('You win!');
+    }
   });
 })();
