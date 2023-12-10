7b903c3342277393fef1b8d640a9fd5d6b4b564c
dyllpickl <144762997+dyllpickl@users.noreply.github.com>
2023-12-09 23:27:00 -0800

    Update graph.js

--- src/cs212/project/final/graph.js
+++ src/cs212/project/final/graph.js
@@ -9,6 +9,17 @@
     }
   };
 
+  function toggleSquareClass(row, col) {
+    var rows = document.querySelectorAll('tr');
+    var numRows = table.length;
+    var numCols = table[0].children.length;
+
+    if (row >= 0 && row < numRows && col >= 0 && col < numCols) {
+      var currentSquare = table[row].children[col];
+      changeClass(currentSquare);
+    }
+  }
+
   function allGrey() {
     var elements = document.querySelectorAll('td');
     var allGrey = true;
@@ -23,7 +34,17 @@
       
                                  
   $('td').on('click', function() {
+    var rows = $(this).parent().index();
+    var cols = $(this).index();
+    var north = (rows - 1, cols);
+    var east = (rows, col + 1);
+    var south = (rows + 1, col);
+    var west = (rows, col - 1);
     changeClass(this);
+    changeClass(north);
+    changeClass(east);
+    changeClass(south);
+    changeClass(west);
     if (allGrey()) {
       window.alert('You win!');
     }
